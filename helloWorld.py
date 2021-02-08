364f52b26e00b65c138d3b3ffdacf732808ff5ea
Cathy-Kat <77729571+Cathy-Kat@users.noreply.github.com>
2021-02-08 20:06:04 +0000

    Add files via upload
    Week 02

---
+++ helloWorld.py
@@ -0,0 +1,4 @@
+# Program that greets the user
+# Author: Cathy Walsh
+
+print ('Hello World! \nHow are you today')
---
+++ readName.py
@@ -0,0 +1,8 @@
+# read in a name and print it out
+# Author: Cathy Walsh
+
+name = input('enter your name:')
+print ('Hello ' + name)
+
+age = input('What is your age?:')
+print ('your age is {}'.format(age))
